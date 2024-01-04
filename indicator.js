c29d086bb22452fa45bf6ad7a37771754163eeee
FBrandex <104596852+FBrandex@users.noreply.github.com>
2024-01-04 11:15:49 -0300

    Add files via upload
    Initial commit of indicador-script.js

---
+++ indicator.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Função para adicionar o parâmetro 'indicador' à URL
+    function addIndicatorParameter(url) {
+        var indicator = new URLSearchParams(window.location.search).get('indicador');
+        if (!indicator) return url;
+
+        var newUrl = new URL(url, window.location.origin);
+        newUrl.searchParams.set('indicador', indicator);
+        return newUrl.href;
+    }
+
+    // Função para tratar o clique
+    function handleLinkClick(event) {
+        var target = event.target;
+
+        // Encontrar o elemento <a> ou <button> mais próximo
+        while (target && target.nodeName !== 'A' && target.nodeName !== 'BUTTON') {
+            target = target.parentNode;
+        }
+
+        if (target) {
+            var destinationUrl = target.nodeName === 'A' ? target.href : target.getAttribute('data-href');
+            if (destinationUrl) {
+                event.preventDefault();
+                window.location.href = addIndicatorParameter(destinationUrl);
+            }
+        }
+    }
+
+    // Adiciona o event listener a todos os links e botões
+    document.querySelectorAll('a, button').forEach(function(element) {
+        element.addEventListener('click', handleLinkClick);
+    });
+});
